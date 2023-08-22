eeed499394fda55d16b7f1cd9e754f14b515d99d
Omar Hafez <omarhafez@Omars-MacBook-Pro-2.local>
2023-08-22 10:26:29 -0400

    more about

--- app/sections/About.tsx
+++ app/sections/About.tsx
@@ -68,6 +68,26 @@ const About = () => {
                             delay={0.2}
                             text=" - Western Governors University 2024, M.S. Information Technology Management "
                         />
+                        <AnimatedTitle
+                            text={"Certifications:"}
+                            className={
+                                "mb-10 text-left text-[40px] font-bold leading-[0.9em] tracking-tighter text-[#e4ded7] sm:text-[45px] md:mb-16 md:text-[60px] lg:text-[80px]"
+                            }
+                            wordSpace={"mr-[14px]"}
+                            charSpace={"mr-[0.001em]"}
+                        />
+                        <AnimatedBody
+                            delay={0.2}
+                            text=" - CompTIA A+ "
+                        />
+                        <AnimatedBody
+                            delay={0.2}
+                            text=" - CompTIA Network+ "
+                        />
+                        <AnimatedBody
+                            delay={0.2}
+                            text=" - CompTIA Security+ "
+                        />
 
                     </div>
                 </div>
